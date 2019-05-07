268adf5356bbc84e95d00bc75e88145b66e0b7bc
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-05-07 09:29:21 +0200

    Updates

---
+++ precache-manifest.eb5061e3a1f3e298dd824fad18cdfa18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485a12fd6d1651434e152c67d0e08c1f",
+    "url": "/Post-Your-Form/index.html"
+  },
+  {
+    "revision": "e098b9d1c70ca925c481",
+    "url": "/Post-Your-Form/static/css/main.04bdc0d4.chunk.css"
+  },
+  {
+    "revision": "64044f925cb6f40e12b0",
+    "url": "/Post-Your-Form/static/js/2.53622f50.chunk.js"
+  },
+  {
+    "revision": "e098b9d1c70ca925c481",
+    "url": "/Post-Your-Form/static/js/main.b0732154.chunk.js"
+  },
+  {
+    "revision": "8023a49e51dca194d950",
+    "url": "/Post-Your-Form/static/js/runtime~main.5b2995f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Life-Cycles/precache-manifest.4e3d1e68af0febf059174718112c8861.js"
+  "/Post-Your-Form/precache-manifest.eb5061e3a1f3e298dd824fad18cdfa18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Life-Cycles/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Post-Your-Form/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
